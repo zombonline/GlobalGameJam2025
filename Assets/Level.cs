effccbbc7bd80546c66d3ca256b9f7e16bb59d39
Bradley's Laptop <megazombie130@gmail.com>
2025-01-25 12:31:46 +0000

    Updated Level prefab to allow for easy scaling

--- Assets/Level.cs
+++ Assets/Level.cs
@@ -2,8 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Level : MonoBehaviour
 {
     [SerializeField] public Transform[] bubbleSpawns, spikeSpawns;
     [SerializeField] public float timeLimit;
+    [SerializeField] private float levelScale = 9f;
+
+    [SerializeField] Camera mainCamera;
+    [SerializeField] Transform outerWalls;
+    [ExecuteInEditMode]
+
+    private void OnValidate()
+    {
+        mainCamera.orthographicSize = levelScale;
+        outerWalls.localScale = new Vector3(levelScale,levelScale,1);
+
+    }
+
+
 }
